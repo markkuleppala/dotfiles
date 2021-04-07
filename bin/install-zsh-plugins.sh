75b6d8ada8632dcf659f69ca96217c2bb800133e
Timo Riski <25483483+rriski@users.noreply.github.com>
2021-04-07 23:26:38 +0300

    Fix powerlevel10k path

--- bin/install-zsh-plugins.sh
+++ bin/install-zsh-plugins.sh
@@ -3,5 +3,5 @@
 source .zshrc
 
 git clone --depth=1 https://github.com/agkozak/zsh-z $ZSH_CUSTOM/plugins/zsh-z
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/.oh-my-zsh/custom}/themes/powerlevel10k
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
 git clone --depth=1 https://github.com/wbingli/zsh-wakatime.git $ZSH_CUSTOM/plugins/zsh-wakatime
