3961bb733cbd69d8ec839b0f44e8a78b5c70e542
Timo Riski <25483483+rriski@users.noreply.github.com>
2021-07-01 14:17:32 +0300

    pyenv: fix conf

--- .shell/external.sh
+++ .shell/external.sh
@@ -16,8 +16,9 @@ export PATH="$PATH:$HOME/.rvm/bin"
 [[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm" # Load RVM into a shell session *as a function*
 
 # pyvenv
-export PATH="$HOME/.pyenv/bin:$PATH"
-eval "$(pyenv init -)"
+export PYENV_ROOT="$HOME/.pyenv"
+export PATH="$PYENV_ROOT/bin:$PATH"
+eval "$(pyenv init --path)"
 
 # Python packages
 export PATH="$PATH:$HOME/Library/Python/3.9/bin"
